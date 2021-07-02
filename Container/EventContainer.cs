d54e33bb87043c68713ed6317253dedd349952fe
Reiga <remrem4862@gmail.com>
2021-07-03 01:19:10 +0900

    Add Assets

---
+++ Container/EventContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoi4ModdingSupporter.Container
+{
+    [DataContract(Name = "EventContainer")]
+    class EventContainer
+    {
+        [DataMember(Name = "id")]
+        private string ID { get; set; }
+
+        [DataMember(Name = "title")]
+        private string Title { get; set; }
+
+        [DataMember(Name = "desc")]
+        private string Description { get; set; }
+    }
+}
---
+++ Util/Parser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoi4ModdingSupporter.Util
+{
+    class Parser
+    {
+        public static void parse()
+        {
+
+        }
+    }
+}
