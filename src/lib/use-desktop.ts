1a8df5770480356a3fc8b500e2e781eff72e3071
Ign1s-Reiga <remrem4862@gmail.com>
2026-09-18 21:36:00 +0900

    refactor(lib): import through the path alias like the rest of the app

--- src/lib/use-desktop.ts
+++ src/lib/use-desktop.ts
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 
-import { isDesktop } from './ipc';
+import { isDesktop } from '@/lib/ipc';
 
 /** The bridge is either there or it is not; it never appears later. */
 const subscribe = () => () => {};
